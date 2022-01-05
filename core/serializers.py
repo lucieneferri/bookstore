1c4a80698c0411fda31fceece158edd3a1e1dbf0
lucieneferri <78495231+lucieneferri@users.noreply.github.com>
2022-01-05 14:46:22 -0300

    Criacao sistema de autenticacao para acessar admin

--- core/serializers.py
+++ core/serializers.py
@@ -57,13 +57,57 @@ class OrderSerializer(serializers.ModelSerializer):
         ]
 
 class UsuarioSerializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        label="Senha"
+    )
+
+    password_confirm = serializers.CharField(
+        style={'input_type': 'password'},
+        write_only=True,
+        label="Confirme a senha"
+    )
+
+    is_staff = serializers.BooleanField(
+        label="Membro da Equipe",
+        help_text="Indica que usuário consegue acessar o site de administração."
+    )
+
+    is_superuser = serializers.BooleanField(
+        label="SuperUsuário",
+        help_text="Indica que este usuário tem todas as permissões sem atribuí-las explicitamente."
+    )
+    
+    
     class Meta:
         model = Usuario
         fields = [
             'id',
             'username',
             'password',
+            'password_confirm',
             'email',
             'is_staff',
             'is_superuser'
         ]
+    def save(self):
+
+        conta = Usuario(
+            email = self.validated_data['email'],
+            username = self.validated_data['username'],
+            is_staff = self.validated_data['is_staff'],
+            is_superuser = self.validated_data['is_superuser']
+        )
+
+        password = self.validated_data['password']
+        password_confirm = self.validated_data['password_confirm']
+
+        if password != password_confirm:
+            raise serializers.ValidationError({'password':'As senhas não são iguais.'})
+        else:
+            conta.set_password(password)
+            conta.save()
+            return conta
+    
